748f8eeea19fffbd1fde9fca85c740e5b7e57e59
Moustafa Mahmoud <moustafafci25@gmail.com>
2021-01-19 23:02:52 +0200

    feat: add nav menu

--- js/app.js
+++ js/app.js
@@ -1,57 +1,61 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
+const navbarList = document.querySelector("#navbar__list");
+const main = document.querySelector("main");
+
+// get all sections in the page
+const sections = main.querySelectorAll("section");
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
 // build the nav
-
-
+const fragment = document.createDocumentFragment();
+sections.forEach((section) => {
+  let item = document.createElement("li");
+  let link = document.createElement("a");
+  link.setAttribute("href", "#");
+  link.setAttribute('data-section' , section.getAttribute('id'));
+  link.textContent = section.getAttribute("data-nav");
+  item.appendChild(link);
+  fragment.appendChild(item);
+});
+navbarList.appendChild(fragment);
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
-
+const links = navbarList.querySelectorAll('a');
+links.forEach(link=>{
+    link.addEventListener('click',e=>{
+        e.preventDefault();
+        console.log(e.target.dataset.section)
+        document.querySelector('#'+e.target.dataset.section).scrollIntoView({behavior:"smooth"})
+    });
+});
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
